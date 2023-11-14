15237fd21374e4ab2a759fc6e7b98b54dbc5f421
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-14 16:06:42 -0400

    Environment vars for JWT, improved context and error handling, Bearer token support

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,37 +1,41 @@
 // utils/auth.js
 const jwt = require('jsonwebtoken');
-const secret = 'mysecretsshhhhh'; // Replace with your secret
-const expiration = '2h';
+
+// It's a best practice to store secret keys and expiration in environment variables
+const secret = process.env.JWT_SECRET || 'mysecretsshhhhh'; // Replace with your secret from environment variables
+const expiration = process.env.JWT_EXPIRATION || '2h';
 
 module.exports = {
-  authMiddleware: function (context) {
-    // Extracting req from context, which is provided in GraphQL setup
-    const req = context.req || context.request;
-
+  authMiddleware: function ({ req }) {
+    // New: Destructure 'req' directly from the parameter to simplify the code
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     if (req.headers.authorization) {
+      // Strip out 'Bearer ' from 'Bearer <token>'
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      // In GraphQL, we don't necessarily want to return a response, so we just return the context
-      return context;
+      // Return context without user if no token found
+      return { user: null };
     }
 
     try {
-      // Verifying the token
+      // Decode and attach user data to the context
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      // Adding the user data to the context
-      context.user = data;
+      req.user = data;
     } catch {
-      console.log('Invalid token');
-      // Here too, just return the context
+      // Log the error and return context without user
+      console.error('Invalid token');
+      req.user = null;
     }
 
-    return context;
+    // Return the request object so we can access the user in the resolver context
+    return req;
   },
+
   signToken: function ({ username, email, _id }) {
+    // Payload should contain user details necessary for your application
     const payload = { username, email, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
